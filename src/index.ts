15f5a64cdd64ef9cb7ec93b401c63de8c79681bf
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-04-30 22:09:43 +0900

    feat:test css build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import "./style.css";
+
 function component() {
     const element = document.createElement("div");
     element.innerHTML = "Hello Webpack";
+    element.classList.add("hello");
     return element;
 }
 
