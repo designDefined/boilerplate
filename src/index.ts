6bc935fc368d5adeaad0f8a8db8685123fa73351
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-04-30 22:18:58 +0900

    feat:test sass-loader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./sass.scss";
 
 function component() {
     const element = document.createElement("div");
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
     module:{
         rules:[
             {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
                 test:/\.css$/i,
                 use:["style-loader", "css-loader"]
             }
