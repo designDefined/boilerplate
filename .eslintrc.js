121aa47980347412627de7f5e98de265426a6893
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-05-02 16:12:00 +0900

    feat:apply prettier to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    root:true,
-    env:{
-        browser:true,
-        node:true,
-    },
-    extends: ["airbnb"]
-}
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["airbnb", "prettier"],
+};
