0755982cad060ef903f5074055e944c881a05909
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-05-02 16:14:36 +0900

    feat:reformat existing codes

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,13 @@ import "./sass.scss";
 import Test from "./assets/test.png";
 
 function component() {
-    const element = document.createElement("div");
-    element.innerHTML = "Hello Webpack";
-    element.classList.add("hello");
-    const testImage = new Image();
-    testImage.src=Test;
-    element.appendChild(testImage);
-    return element;
-
+  const element = document.createElement("div");
+  element.innerHTML = "Hello Webpack";
+  element.classList.add("hello");
+  const testImage = new Image();
+  testImage.src = Test;
+  element.appendChild(testImage);
+  return element;
 }
 
 document.body.appendChild(component());
-
